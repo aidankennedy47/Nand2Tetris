74da57f0dfb559c34100fd51dbe8dfd6c3870c15
aidankennedy47 <aidankennedy47@gmail.com>
2025-09-25 20:51:30 +1000

    absolute part finished

--- Abs/Abs.asm
+++ Abs/Abs.asm
@@ -1,4 +1,20 @@
 // Calculates the absolute value of R1 and stores the result in R0.
 // (R0, R1 refer to RAM[0], and RAM[1], respectively.)
 
-// Put your code here.
+@R1
+D = M
+
+@POSITIVE
+D;JGE
+
+D = -D
+@R0
+M = D
+@END
+0;JMP
+
+(POSITIVE)
+@R0
+M = D
+
+(END)

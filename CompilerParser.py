bbf4c925c7783e9b7df4fd4c57f78c5df36cb2de
aidankennedy47 <aidankennedy47@gmail.com>
2025-11-05 13:35:20 +1100

    fixed up parameterlist

--- CompilerParser.py
+++ CompilerParser.py
@@ -120,11 +120,9 @@ class CompilerParser :
         while True:
             if self.have('keyword', 'int') or self.have('keyword', 'char') or self.have('keyword', 'boolean'):
                 tree.addChild(self.mustBe('keyword', self.current().getValue()))
-            elif self.have('identifier', self.current().getValue()):
-                tree.addChild(self.mustBe('identifier', self.current().getValue()))
             else:
-                raise ParseException("Expected type in parameterList")
-
+                tree.addChild(self.mustBe('identifier', self.current().getValue()))
+            
             tree.addChild(self.mustBe('identifier', self.current().getValue()))
 
             if self.have('symbol', ','):
